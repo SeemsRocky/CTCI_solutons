476ffd73f1f808c623d41d7319d02a067fffb33c
SeemsRocky <rliao3613@gmail.com>
2020-03-23 17:56:00 -0400

    changed detailed comments

--- Array And Strings/isUnique.js
+++ Array And Strings/isUnique.js
@@ -1,8 +1,11 @@
-/*
-  Implement an algorithm if a string has all unique characters
-  Assuming S only contains ASCII characters and length is 0 or greater
-  Space Complexity - O(n)
-  Time Complexity - O(n)
+/** 
+* Implement an algorithm if a string has all unique characters
+* Assuming S only contains ASCII characters and length is 0 or greater
+* Space Complexity - O(n)
+* Time Complexity - O(n)
+* @param {string} s1
+* @param {string} s2
+* @return {boolean}
 */
 function isUnique(s){
   const dict = {}
